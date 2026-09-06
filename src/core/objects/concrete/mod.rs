b120fe459a80c9ec983c9af3cc1a42e3429b9079
Kaique Pereira <cubolateral@gmail.com>
2026-09-06 17:05:41 -0300

    refactor: group concrete objects under a dedicated module

---
+++ src/core/objects/concrete/mod.rs
@@ -0,0 +1,17 @@
+mod camera;
+mod circle;
+mod group;
+mod latex;
+mod line;
+mod rect;
+mod text;
+mod triangle;
+
+pub use camera::*;
+pub use circle::*;
+pub use group::*;
+pub use latex::*;
+pub use line::*;
+pub use rect::*;
+pub use text::*;
+pub use triangle::*;
